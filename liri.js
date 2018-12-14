bf9d242dc47c1e7eabc9dace8e833c1a359dc67c
LidiaWebDev <lidia@fishisfast.com>
2018-12-13 22:29:53 -0800

    raw

---
+++ liri.js
@@ -0,0 +1,131 @@
+require("dotenv").config();
+
+// get Spotify keys
+var spotify = new Spotify(keys.spotify);
+
+// Writes to the log.txt file
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+
+var axios = require("axios");
+var artist = process.argv[2];
+var queryUrl =
+  "https://rest.bandsintown.com/artists/" +
+  artist +
+  "/events?app_id=codingbootcamp";
+console.log(queryUrl);
+axios.get(queryUrl).then(function(response) {
+  console.log(response.data);
+  console.log("Name of the venue: " + response.data.Name);
+  console.log("Venue location:" + response.data.Location);
+  console.log("Date of the Event :" + response.data.MM / DD / YYYY);
+});
+
+// Function for running a Spotify search - Command is spotify-this-song
+var getSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "I Want it That Way";
+  }
+
+  spotify.search(
+    {
+      type: "track",
+      query: userCommand
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+
+      var songs = data.tracks.items;
+
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
+    }
+  );
+};
+
+//Switch command
+function mySwitch(userCommand) {
+  //choose which statement (userCommand) to switch to and execute
+  switch (userCommand) {
+    case "concert-this":
+      getConcert();
+      break;
+
+    case "spotify-this-song":
+      getSpotify();
+      break;
+
+    case "movie-this":
+      getMovie();
+      break;
+
+    case "do-what-it-says":
+      doWhat();
+      break;
+  }
+
+  //OMDB Movie - command: movie-this
+  function getMovie() {
+    // OMDB Movie - this MOVIE base code is from class files, I have modified for more data and assigned parse.body to a Var
+    var movieName = secondCommand;
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl =
+      "http://www.omdbapi.com/?t=" +
+      movieName +
+      "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+    request(queryUrl, function(error, response, body) {
+      // If the request is successful = 200
+      if (!error && response.statusCode === 200) {
+        var body = JSON.parse(body);
+        //Simultaneously output to console and log.txt via NPM simple-node-logger
+        logOutput("================ Movie Info ================");
+        logOutput("Title: " + body.Title);
+        logOutput("Release Year: " + body.Year);
+        logOutput("IMdB Rating: " + body.imdbRating);
+        logOutput("Country: " + body.Country);
+        logOutput("Language: " + body.Language);
+        logOutput("Plot: " + body.Plot);
+        logOutput("Actors: " + body.Actors);
+        logOutput("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
+        logOutput("Rotten Tomatoes URL: " + body.tomatoURL);
+        logOutput("==================THE END=================");
+      } else {
+        //else - throw error
+        console.log("Error occurred.");
+      }
+      //Response if user does not type in a movie title
+      if (movieName === "Mr. Nobody") {
+        console.log("-----------------------");
+        console.log(
+          "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/"
+        );
+        console.log("It's on Netflix!");
+      }
+    });
+  }
+  //Function for command do-what-it-says; reads and splits random.txt file
+  //command: do-what-it-says
+  function doWhat() {
+    //Read random.txt file
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (!error);
+      console.log(data.toString());
+      //split text with comma delimiter
+      var cmds = data.toString().split(",");
+    });
+  }
+} //Closes mySwitch func - Everything except the call must be within this scope
+
+//Call mySwitch function
+mySwitch(userCommand);
